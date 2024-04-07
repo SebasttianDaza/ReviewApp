83d46a32c060676fd968e1ec7fb713ee4a38df90
juadaz <juadaz@eltiempo.com>
2024-04-06 23:02:01 -0500

    build: Update database service docker

--- publisher/views.py
+++ publisher/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def index(request: WSGIRequest) -> HttpResponse:
+    return HttpResponse("Hello, world. You're at the Review Index page.")
