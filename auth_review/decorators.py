2a96af6607f2c0c541a06fb04db256667d72a4f6
juadaz <juadaz@eltiempo.com>
2025-04-07 14:09:23 -0500

    feat: RV-Auth Add try catch to view and decorators

--- auth_review/decorators.py
+++ auth_review/decorators.py
@@ -73,43 +73,52 @@ def oauth_twitter_access_token(view_func):
         params = request.GET
 
         if params.get(key="oauth_token") and params.get(key="oauth_verifier"):
-            import time
-            from auth_review.http.request import get_signature_twitter
-            time_signature = round(time.time())
+            try:
+                import time
+                from auth_review.http.request import get_signature_twitter
+                time_signature = round(time.time())
 
-            oauth_verifier = params.get(key="oauth_verifier")
-            oauth_token = params.get(key="oauth_token")
-            auth = [
-                f'oauth_consumer_key={env("API_KEY_TWITTER")}',
-                'oauth_nonce=ea9ec8429b68d6b77cd5600adbbb0456',
-                'oauth_signature_method=HMAC-SHA1',
-                f'oauth_timestamp={time_signature}',
-                f'oauth_token={oauth_token}',
-                'oauth_version=1.0'
-            ]
-            url = f"{env('API_TWITTER')}/oauth/access_token"
-            auth.insert(3, f'oauth_signature="{get_signature_twitter(url, auth)}"')
+                oauth_verifier = params.get(key="oauth_verifier")
+                oauth_token = params.get(key="oauth_token")
+                auth = [
+                    f'oauth_consumer_key={env("API_KEY_TWITTER")}',
+                    'oauth_nonce=ea9ec8429b68d6b77cd5600adbbb0456',
+                    'oauth_signature_method=HMAC-SHA1',
+                    f'oauth_timestamp={time_signature}',
+                    f'oauth_token={oauth_token}',
+                    'oauth_version=1.0'
+                ]
+                url = f"{env('API_TWITTER')}/oauth/access_token"
+                auth.insert(3, f'oauth_signature="{get_signature_twitter(url, auth)}"')
 
-            headers = {
-                "Authorization": "{0} {1}".format("OAuth", ", ".join(auth)),
-                "Content-Type": "application/x-www-form-urlencoded"
-            }
-            res = requests.post(f"{url}?oauth_verifier={oauth_verifier}", headers=headers)
+                headers = {
+                    "Authorization": "{0} {1}".format("OAuth", ", ".join(auth)),
+                    "Content-Type": "application/x-www-form-urlencoded"
+                }
+                res = requests.post(f"{url}?oauth_verifier={oauth_verifier}", headers=headers)
 
-            if res.status_code == 200:
-                params_verifier = QueryDict(res.text)
-                args = args + ({
-                    "oauth_token": params_verifier.get(key="oauth_token"),
-                    "oauth_token_secret": params_verifier.get(key="oauth_token_secret"),
-                    "user_id": params.get(key="user_id"),
-                    "time_signature": time_signature
-                },)
-                response = view_func(request, *args, **kwargs)
-                return response
-            else:
+                if res.status_code == 200:
+                    params_verifier = QueryDict(res.text)
+                    args = args + ({
+                        "oauth_token": params_verifier.get(key="oauth_token"),
+                        "oauth_token_secret": params_verifier.get(key="oauth_token_secret"),
+                        "user_id": params.get(key="user_id"),
+                        "time_signature": time_signature
+                    },)
+                    response = view_func(request, *args, **kwargs)
+                    return response
+                else:
+                    raise requests.exceptions.RequestException(res.text)
+            except (
+                    requests.exceptions.RequestException or
+                    requests.exceptions.ConnectionError or
+                    requests.exceptions.Timeout or
+                    requests.exceptions.TooManyRedirects
+            ) as e:
                 logger.warn(
-                    f"Exception raised while requesting twitter access token {res.text}"
+                    f"Exception raised while requesting twitter access token {e}"
                 )
+                pass
 
         return redirect("/admin/login")
 
--- auth_review/views.py
+++ auth_review/views.py
@@ -43,50 +43,61 @@ class LoginView(DjangoLoginView):
 
 @oauth_twitter_access_token
 def oauth_twitter_login(request: HttpRequest, *args):
-    params = args[0] if args else {}
-    url = f"{env('API_TWITTER_1_0')}/account/verify_credentials.json?include_email=true"
-    auth = [
-        f'oauth_consumer_key={env("API_KEY_TWITTER")}',
-        'oauth_nonce=ea9ec8429b68d6b77cd5600adbbb0456',
-        'oauth_signature_method=HMAC-SHA1',
-        f'oauth_timestamp={params.get("time_signature")}',
-        f'oauth_token={params.get("oauth_token")}',
-        'oauth_version=1.0'
-    ]
-    auth.insert(
-        3,
-        f'oauth_signature="{get_signature_twitter(url, auth, "GET", params.get("oauth_token_secret"))}"'
-    )
+    try:
+        params = args[0] if args else {}
+        url = f"{env('API_TWITTER_1_0')}/account/verify_credentials.json?include_email=true"
+        auth = [
+            f'oauth_consumer_key={env("API_KEY_TWITTER")}',
+            'oauth_nonce=ea9ec8429b68d6b77cd5600adbbb0456',
+            'oauth_signature_method=HMAC-SHA1',
+            f'oauth_timestamp={params.get("time_signature")}',
+            f'oauth_token={params.get("oauth_token")}',
+            'oauth_version=1.0'
+        ]
+        auth.insert(
+            3,
+            f'oauth_signature="{get_signature_twitter(url, auth, "GET", params.get("oauth_token_secret"))}"'
+        )
 
-    user_data = requests.get(
-        url,
-        headers={
-            "Authorization": "{0} {1}".format("OAuth", ", ".join(auth)),
-            "Content-Type": "application/json"
-        }
-    )
+        user_data = requests.get(
+            url,
+            headers={
+                "Authorization": "{0} {1}".format("OAuth", ", ".join(auth)),
+                "Content-Type": "application/json"
+            }
+        )
 
-    if user_data.status_code == 200:
-        user_data = QueryDict(user_data.text)
-        username = user_data.get(key="screen_name")
-        first_name, last_name, *rest = user_data.get(key="name").split(" ")
-        user = AuthUser.objects.get(username=username)
-        if user is None:
-            AuthUser.objects.create_user(
-                username=username,
-                first_name=first_name,
-                last_name=last_name,
-                email=user_data.get(key="email"),
-                password="admin"
-            )
+        if user_data.status_code == 200:
+            user_data = QueryDict(user_data.text)
+            username = user_data.get(key="screen_name")
+            first_name, last_name, *rest = user_data.get(key="name").split(" ")
+            user = AuthUser.objects.get(username=username)
+            if user is None:
+                AuthUser.objects.create_user(
+                    username=username,
+                    first_name=first_name,
+                    last_name=last_name,
+                    email=user_data.get(key="email"),
+                    password="admin"
+                )
 
-        user = authenticate(username=username, password="admin")
-        return login(request, user)
+            user = authenticate(username=username, password="admin")
+            return login(request, user)
+        else:
+            raise requests.exceptions.RequestException(user_data.text)
+    except (
+            requests.exceptions.RequestException or
+            requests.exceptions.ConnectionError or
+            requests.exceptions.Timeout or
+            requests.exceptions.TooManyRedirects
+    ) as e:
+        logger.warn(
+            f"Exception raised while requesting twitter verify credentials {e}"
+        )
+        pass
 
-    logger.warn(
-        f"Exception raised while requesting twitter verify credentials {user_data.text}"
-    )
     return redirect("/admin/login")
+
 
 
 def terms_of_service():
